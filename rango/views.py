65aa984072a45f30e84c1286e986af7f86c05a1a
Andrew Parker <239622P@student.gla.ac.uk>
2020-02-12 20:32:00 +0000

    Chapter4

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from tango_with_django_project import settings
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'myname': 'Andrew Parker'}
+    return render(request, 'rango/about.html', context=context_dict)
