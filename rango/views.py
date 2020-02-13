de31b2d5f570c4169d6279fed828b755e8962c5c
Andrew Parker <239622P@student.gla.ac.uk>
2020-02-13 20:40:18 +0000

    Chapter7

--- rango/views.py
+++ rango/views.py
@@ -50,7 +50,7 @@ def add_category(request):
             return redirect ('/rango/')
         else:
             print(form.errors)
-    return render(request, 'rango/add_category.html', {'form': form})
+    return render(request, "rango/add_category.html", {'form': form})
 
 def add_page(request, category_name_slug):
     try:
